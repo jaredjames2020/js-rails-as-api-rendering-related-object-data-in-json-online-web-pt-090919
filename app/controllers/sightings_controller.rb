935ce127cd47a2b09a3fb168eaf2a03219d8a382
Jared S <jaredjames2020@gmail.com>
2020-04-09 14:22:05 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -10,7 +10,7 @@ class SightingsController < ApplicationController
        render json: sighting.to_json(include: [:bird, :location])
       else
        render json: { message: 'No sighting found with that id' }
-    end
+      end
   end
     
 end
